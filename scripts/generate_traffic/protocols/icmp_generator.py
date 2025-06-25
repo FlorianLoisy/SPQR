9896e7a704b27b7512ba2595443083f7f5f28c47
Popanov <flocy@hotmail.fr>
2025-06-25 09:40:01 +0200

    modification icmp_generator

--- scripts/generate_traffic/protocols/icmp_generator.py
+++ scripts/generate_traffic/protocols/icmp_generator.py
@@ -66,3 +66,39 @@ class ICMPGenerator(ProtocolGenerator):
                 time.sleep(self.time_interval / 1000)
         
         return packets
+    
+    def generate_icmp_from_schedule(json_path: str, output_pcap: str):
+        
+        json_path = Path("config/config.json")
+        with json_path.open() as f:
+           data = json.load(f)
+
+        icmp_specifique = data["traffic_patterns"]["icmp_specifique"]
+        dst_ip = icmp_specifique["parameters"]["dst_ip"]
+        schedule = icmp_specifique["parameters"]["schedule"]
+
+        ping_events = []
+
+        for entry in schedule:
+            src_ip = entry["src_ip"]
+            for ts in entry["timestamps"]:
+                timestamp = datetime.fromisoformat(ts)
+                ping_events.append((timestamp, src_ip))
+
+        # Trier les événements dans l'ordre chronologique
+        ping_events.sort()
+
+        print(f"Nombre total de pings planifiés : {len(ping_events)}")
+
+        packets = []
+
+        # Boucle principale d'attente et d'exécution
+        for ts, src_ip in ping_events:
+            pkt = IP(src=src_ip, dst=dst_ip) / ICMP() / b"ping-test"
+            pkt.time = time.mktime(ts.timetuple())  # Assigner le timestamp au paquet
+            packets.append(pkt)
+
+            # Écrire les paquets dans le fichier pcap
+        wrpcap(output_pcap, packets)
+
+        print(f"{len(packets)} paquets ICMP ont été écrits dans '{output_pcap}'")
